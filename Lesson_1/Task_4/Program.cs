6ad48cc23c1f4a370c6624dadc32dc2f71912e6b
VR <rozhkovvv@gmail.com>
2022-12-15 23:05:13 +0300

    Lesson 1 - Task 4

---
+++ Lesson_1/Task_4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Введите второе число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Введите третье число: ");
+int Number3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int Max = Number1;
+
+if ( Number2 > Max )
+    Max = Number2;
+if (Number3 > Max )
+    Max = Number3;
+
+Console.Write("Максимальное число: " + Max);
+Console.WriteLine();
+Console.WriteLine();
