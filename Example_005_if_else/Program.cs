8238b14bddb0c7b8be4db22b7f4d38f4f119a58f
VR <rozhkovvv@gmail.com>
2022-12-15 15:57:34 +0300

    Ex_005

---
+++ Example_005_if_else/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine();
+Console.Write("Введите имя пользователя: ");
+
+string userName = Console.ReadLine();
+Console.WriteLine();
+
+if ( userName.ToLower() == "маша" )
+{
+    Console.WriteLine("УРА, это Маша!!!");
+}
+else
+{
+    Console.WriteLine ("Привет, " + userName + "!" );
+}
+Console.WriteLine ();
+
