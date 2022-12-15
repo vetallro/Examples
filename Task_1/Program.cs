fb395c7a4cd436c4134a9eb375714d80e5352240
VR <rozhkovvv@gmail.com>
2022-12-15 20:54:52 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int Result = Number * Number;
+int ResultMath = Convert.ToInt32(Math.Pow(Number,2));
+
+Console.Write("Ответ: ");
+Console.WriteLine(Result);
+Console.Write("И еще ответ: ");
+Console.WriteLine(ResultMath);
+
+System.Console.WriteLine();
+
+
