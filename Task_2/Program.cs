d57e87f6ae26abe48542ce1552dad56f5c9d6d74
VR <rozhkovvv@gmail.com>
2022-12-15 21:04:11 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine();
+Console.Write("Введите первое число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Введите второе число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int ResultMath = Convert.ToInt32(Math.Pow(Number2, 2));
+
+if ( Number1 == ResultMath )
+    {
+        Console.Write("ДА");
+    }
+else
+    {
+        Console.Write("НЕТ");
+    }
+Console.WriteLine();
