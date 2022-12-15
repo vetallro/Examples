09659909d0836d9cb4cee87244dde07a6f1f3d27
VR <rozhkovvv@gmail.com>
2022-12-15 21:34:59 +0300

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine();
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int AlterNumber = - (Number);
+int i = AlterNumber;
+while (i <= Number)
+{
+    Console.Write(i + " ");
+    i++;
+}
+Console.WriteLine();
+Console.WriteLine();  
