2f926fd991c05eed439c568bbb9679db28a3a148
VR <rozhkovvv@gmail.com>
2022-12-15 22:40:38 +0300

    Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Clear();
+Console.WriteLine();
+Console.Write("Введите число для вывода таблицы умножения: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+for (int Index = 1; Index <=10; Index++) 
+{
+    int Result = Number * Index;
+    Console.Write(Number + " * ");
+    Console.Write(Index + " = ");
+    Console.Write(Result);
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+// телеграмм преподавателя @slanderc
