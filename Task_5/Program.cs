cdd593211f53847ecfb796fc2859ee86418e6fda
VR <rozhkovvv@gmail.com>
2022-12-15 21:50:22 +0300

    Task_5

---
+++ Task_5/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine();
+Console.Write("Введите трехзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int LastNumber = Number - Number/10*10;
+int LastNumber2 = Number % 10;
+
+Console.WriteLine(LastNumber);
+Console.WriteLine(LastNumber2);
+
+// телеграмм преподавателя @slanderc
