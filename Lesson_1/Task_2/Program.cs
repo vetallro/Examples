f0f8bc6753ab08ddc7e1271058bb042045249d8e
VR <rozhkovvv@gmail.com>
2022-12-15 22:58:08 +0300

    Lesson 1 - Task 2

---
+++ Lesson_1/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Введите второе число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+if ( Number1 != Number2 )
+    if ( Number1 > Number2 )
+            Console.Write("Первое число больше второго.");
+    else
+            Console.Write("Второе число больше первого.");
+else
+    Console.Write("Числа равны.");
+Console.WriteLine();
+Console.WriteLine();
