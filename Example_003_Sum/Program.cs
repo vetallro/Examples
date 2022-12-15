ba9665a8813e2599f5deee200f7fe744df4ecb57
VR <rozhkovvv@gmail.com>
2022-12-15 15:36:16 +0300

    Ex_003

---
+++ Example_003_Sum/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine();
+int numA = new Random().Next(1,100);
+Console.Write("Первое число: ");
+Console.WriteLine(numA);
+
+int numB = new Random().Next(1,100);
+Console.Write("Второе число: ");
+Console.WriteLine(numB);
+
+Console.WriteLine();
+
+int Result = numA + numB;
+
+Console.Write("Результат сложения: ");
+Console.WriteLine(Result);
+Console.WriteLine();
