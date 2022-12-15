85758b2864c43c2d21be84891e4709df261419ee
VR <rozhkovvv@gmail.com>
2022-12-15 23:08:52 +0300

    Lesson 1 - Task 6

---
+++ Lesson_1/Task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+if ( Number1 % 2 == 0 )
+    Console.Write("Число четное.");
+else
+    Console.Write("Число нечетное.");
+
+Console.WriteLine();
+Console.WriteLine();
