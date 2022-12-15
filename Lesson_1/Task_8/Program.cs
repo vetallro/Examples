4f0ec23dc1ec99692e6ec967c144f42dc2ee6396
VR <rozhkovvv@gmail.com>
2022-12-15 23:13:44 +0300

    Lesson 1 Task 8

---
+++ Lesson_1/Task_8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+for ( int Index = 2; Index <= Number1; Index = Index + 2 )
+    Console.Write(Index + ", ");
+
+Console.WriteLine();
+Console.WriteLine();
