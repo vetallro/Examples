b20780962a34e70a9566c94f55755c47b0205f42
VR <rozhkovvv@gmail.com>
2022-12-18 23:34:20 +0300

    Ex_010 Find

---
+++ Example_010_find/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = { 1,5,4,7,5,6,3,7,9,8,4 };
+int n = array.Length;
+
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
