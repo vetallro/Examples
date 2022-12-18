be4bfcde758d13e53f7cec09dddd4f039df52214
VR <rozhkovvv@gmail.com>
2022-12-18 21:29:39 +0300

    edit Shft Alt F5

--- Example_005_if_else/Program.cs
+++ Example_005_if_else/Program.cs
@@ -5,13 +5,13 @@ Console.Write("Введите имя пользователя: ");
 string userName = Console.ReadLine();
 Console.WriteLine();
 
-if ( userName.ToLower() == "маша" )
+if (userName.ToLower() == "маша")
 {
     Console.WriteLine("УРА, это Маша!!!");
 }
 else
 {
-    Console.WriteLine ("Привет, " + userName + "!" );
+    Console.WriteLine("Привет, " + userName + "!");
 }
-Console.WriteLine ();
+Console.WriteLine();
 
