c0e96ea854895a7b66451681d5f61bb0daffaa81
VR <rozhkovvv@gmail.com>
2022-12-19 22:54:49 +0300

    Ex_011

---
+++ Example_011_Function/Program.cs
@@ -0,0 +1,53 @@
+﻿void FullArray ( int[] collection )
+{
+    int length = collection.Length;
+    int index = 0;
+
+    while ( index < length )
+    {
+        collection[index] = new Random().Next(1, 5);
+        index++;
+    }
+
+}
+
+void PrintArray( int[] collection )
+{
+    int size = collection.Length;
+    int pozition = 0;
+
+    while ( pozition < size )
+    { 
+        Console.Write(collection[ pozition ] + " ");
+        pozition++;
+    }
+
+}
+
+int IndexOf( int[] collect, int find )
+{
+    int i = collect.Length;
+    int index = 0;
+    int position = -1;
+
+    while ( index < i )
+    {
+        if( collect[index] == find )
+        {
+            position = index;
+            break;
+        }
+      index++;
+    }
+    return position;      
+}
+
+int[] array = new int[20];
+FullArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine( "Первое вхождение " + pos );
