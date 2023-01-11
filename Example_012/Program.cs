71ceeab69a48226b7ddbb9e4d8c21905dff9caa9
VR <rozhkovvv@gmail.com>
2023-01-11 08:57:23 +0300

    Ex 12 - selectionSort

---
+++ Example_012/Program.cs
@@ -0,0 +1,43 @@
+﻿void FullArray ( int[] collection )
+{
+    int length = collection.Length;
+    for (int i = 0; i < length; i++)
+    {
+        collection[i] = new Random().Next(1, 99);
+    }
+}
+
+void PrintArray( int[] collection )
+{
+    Console.WriteLine("---------");
+    int size = collection.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(collection[i] + " ");
+    }
+    Console.WriteLine();
+    Console.WriteLine("---------");
+}
+
+void SelectionSort( int[] coll )
+{
+    for (int i = 0; i < coll.Length; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < coll.Length; j++)
+        {
+            if ( coll[j] < coll[minPosition] )
+                {
+                    int temp = coll[j];
+                    coll[j] = coll[minPosition];
+                    coll[minPosition] = temp;
+                }
+        }
+    }
+}
+
+int[] array = new int[20];
+FullArray(array);
+PrintArray(array);
+SelectionSort(array);
+PrintArray(array);
