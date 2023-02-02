5f322240416d952c2009151e3fff2ffe6cdb4dd5
VR <rozhkovvv@gmail.com>
2023-02-02 08:13:33 +0300

    Lesson 9 homework

---
+++ Lesson_9/Task_64/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int InputNumber(string message)
+{
+    Console.Write(message + ": ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void Countdown(int i)
+{
+    System.Console.Write(i);
+    if (i <= 1)
+    {
+        return;
+    }
+    System.Console.Write(", ");
+    Countdown(i - 1);
+}
+
+int i = InputNumber("Введите число N");
+System.Console.Write($" N = {i} -> \"");
+Countdown(i);
+System.Console.Write($"\"");
---
+++ Lesson_9/Task_66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int InputNumber(string message)
+{
+    Console.Write(message + ": ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int SumNumbers(int m, int n)
+{
+    int sum = n;
+    if (n > m)
+    {
+        sum += SumNumbers(m, n - 1);
+    }
+    return sum;
+}
+
+int m = InputNumber("Введите число M");
+int n = InputNumber("Введите число N");
+
+if (n > m)
+    System.Console.WriteLine($" M = {m}; N = {n} -> {SumNumbers(m, n)}");
+else
+    System.Console.WriteLine($" M = {m}; N = {n} -> {SumNumbers(n, m)}");
