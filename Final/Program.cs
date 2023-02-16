2d9e8889d3d367c17ba0dd4abb9acd8299c6c5cb
VR <rozhkovvv@gmail.com>
2023-02-16 20:56:38 +0300

    final

---
+++ Final/Program.cs
@@ -0,0 +1,12 @@
+﻿// Контрольная работа 
+
+
+// ["hello", "2", "world", ":-)" ] => ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] => ["-2"]
+// [Russia, "Denmark", "Kazan"] => []
+
+
+string[] source = {"hello", "2", "world", ":-)"};
+// string[] source = {"1234", "1567", "-2", "computer science"};
+// string[] source = {Russia, "Denmark", "Kazan"};
+
